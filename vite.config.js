e91be802a55cd6523e886d29d5d876c1c064791c
mavk <urieljtenorio@gmail.com>
2022-07-16 19:43:16 -0500

    Fix error in base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://github.com/mavuriel/react-taskapp'
+  base: 'https://mavuriel.github.io/react-taskapp/',
+  plugins: [react()]
 })
